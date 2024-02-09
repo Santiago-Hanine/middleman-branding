9b236305994f40a0a13ac79b13faa4ed4a1c63a9
Santiago Hanine <santihanine@gmail.com>
2024-02-09 14:38:56 -0300

    Update Navbar component styling

--- src/app/Components/Navbar/index.tsx
+++ src/app/Components/Navbar/index.tsx
@@ -13,29 +13,18 @@ const Navbar = () => {
   const handleMenu = () => {
     const menu = document.querySelector('#menu-mobile')
     menu?.classList.toggle('hidden')
+    document.body.classList.toggle('overflow-hidden')
   }
 
   return (
-    <header className='flex fixed top-0 items-center justify-between px-8 py-4 border-b-blue border-b h-20 w-full'>
+    <header className='flex top-0 items-center justify-between px-8 py-4 border-b-blue border-b h-20 w-full bg-white'>
       <Link href="./">
         <Image src={Logo} width={130} alt='MiddleMan Branding logo'/>
       </Link>
 
       <nav>
-        <ul className='hidden lg:flex text-xl gap-8 items-center'>
+        <ul id='menu-mobile' className='hidden flex text-center lg:flex flex-col lg:flex-row items-start lg:items-center justify-start lg:static absolute top-20 left-0 h-screen bg-white lg:bg-transparent gap-20 lg:gap-10 z-50 w-full pl-8 pt-20 pb-20 lg:pt-0 lg:pl-0 lg:pb-0 text-3xl font-black lg:text-base'>
           {
-            Navlist.map((item, index) => (
-              <li key={index}>
-                <Link href={item.link}>
-                  {item.name}
-                </Link>
-              </li>
-            ))
-          }
-        </ul>
-
-        <ul id='menu-mobile' className='hidden flex flex-col items-center justify-center lg:hidden absolute top-20 left-0 h-screen bg-white z-50 w-full'>
-        {
             Navlist.map((item, index) => (
               <li key={index}>
                 <Link href={item.link}>
