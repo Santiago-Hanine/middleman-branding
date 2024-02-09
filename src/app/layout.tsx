3c9a7ce1e5106b90c3fda6206280f1666b4aed8b
Santiago Hanine <santihanine@gmail.com>
2024-02-09 02:49:34 -0300

    Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,21 +8,10 @@ export const metadata: Metadata = {
   title: "MiddleMan Branding - Impulsa tu éxito en el mundo digital",
   description: "MiddleMan Branding es una agencia de marketing digital líder que se especializa en potenciar tanto a creadores de contenido como a marcas. Impulsa tu éxito en línea con nuestras estrategias innovadoras y colaborativas.",
   keywords: "marketing digital, branding, creadores de contenido, estrategias digitales, agencia de marketing",
-  twitter: {
-    site: "@middlemanbranding",
-    card: "summary_large_image",
-    description: "MiddleMan Branding es una agencia de marketing digital líder que se especializa en potenciar tanto a creadores de contenido como a marcas. Impulsa tu éxito en línea con nuestras estrategias innovadoras y colaborativas.",
-    title: "MiddleMan Branding - Impulsa tu éxito en el mundo digital",
-    images: [
-      {
-        url: "./public/images/logo.jpg",
-        width: 1200,
-        height: 630,
-        alt: "MiddleMan Branding - Impulsa tu éxito en el mundo digital",
-      },
-    ]
-  },
   category: "Marketing",
+  openGraph:{
+    images: ['./public/logo.jpg']
+  }
 }
 
 
