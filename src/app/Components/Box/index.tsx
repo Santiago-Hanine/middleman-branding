4779412a93ae32bc3a3692f15af2cff5f1126762
Santiago Hanine <santihanine@gmail.com>
2024-02-09 17:52:21 -0300

    Refactor Box and Card components

--- src/app/Components/Box/index.tsx
+++ src/app/Components/Box/index.tsx
@@ -13,8 +13,8 @@ interface BoxProps {
 
 const Box = ({eyebrow, title, description, link, children, rowReverse}:BoxProps) => {
   return (
-    <div className={`${rowReverse ? 'lg:flex-row-reverse from-[rgba(45,102,174,0.28)] to-blue' : 'lg:flex-row from-blue to-[rgba(45,102,174,0.28)]'} text-white flex flex-col justify-between lg:items-center my-20 relative mx-4 md:mx-12 lg:mx-24 rounded-[3rem] p-8 bg-gradient-to-r  gap-y-8`}>
-            <h4 className={`absolute ${rowReverse ? 'top-4 right-8' : 'top-4 left-8'} text-xl font-black `}>
+    <div className={`${rowReverse ? 'lg:flex-row-reverse from-[rgba(45,102,174,0.28)] to-blue' : 'lg:flex-row from-blue to-[rgba(45,102,174,0.28)]'} text-white flex flex-col justify-between lg:items-center my-20 relative mx-4 md:mx-12 lg:mx-24 rounded-[3rem] pt-24 pb-8 px-8 lg:p-8 bg-gradient-to-r gap-y-8`}>
+            <h4 className={`absolute ${rowReverse ? 'top-6 right-8' : 'top-6 left-8'} text-xl font-black `}>
                 {eyebrow}
             </h4>
         <div>
@@ -24,18 +24,18 @@ const Box = ({eyebrow, title, description, link, children, rowReverse}:BoxProps)
                     {title}
                 </h2>
 
-                <p data-aos="fade-up" data-aos-duration="500" className='text-xl font-regular max-w-[30ch]'>
+                <p data-aos="fade-up" data-aos-duration="500" className='text-xl font-regular lg:max-w-[30ch]'>
                     {description}
                 </p>
             </div>
         </div>
 
-        <div className='grid grid-cols-2 gap-4 lg:gap-8'>
+        <div className='grid grid-cols-2 gap-8 lg:gap-8'>
             {children}
         </div>
 
-        <Link className={`absolute ${rowReverse ? 'bottom-4 right-8' : 'bottom-4 left-8'}`} href={link}>
-            <button className='hover:border hover:border-black hover:bg-white hover:text-black transform duration-300 ease-in-out p-4 rounded-full bg-black text-white border-black hover:scale-110 animate-appear'>
+        <Link className={`lg:absolute ${rowReverse ? 'bottom-6 right-8' : 'bottom-6 left-8'}`} href={link}>
+            <button className='hover:border w-full hover:border-black hover:bg-white hover:text-black transform duration-300 ease-in-out p-4 rounded-full bg-black text-white border-black hover:scale-110 animate-appear]'>
                 Saber Más...
             </button>
         </Link>
--- src/app/Components/Card/index.tsx
+++ src/app/Components/Card/index.tsx
@@ -8,8 +8,8 @@ interface CardProps {
 
 const Card = ({img, title, aosDuration}: CardProps) => {
   return (
-    <div data-aos="fade-up" data-aos-duration={aosDuration} className='bg-white relative w-72 flex flex-col items-center justify-evenly gap-5 rounded-3xl p-6'>
-        <div className="w-28 flex justify-center items-center">
+    <div data-aos="fade-up" data-aos-duration={aosDuration} className='bg-white relative xl:w-72 flex flex-col items-center justify-evenly md:gap-5 rounded-3xl p-4 md:p-6'>
+        <div className="md:w-28 flex justify-center items-center">
             <img src={img} alt={title} />
         </div>
         <h3 className="text-black text-center  max-w-[19ch]">{title}</h3>
