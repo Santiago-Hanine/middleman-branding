efd5fdad8b257df27d26e93bf41437bbbae8cf0a
Santiago Hanine <santihanine@gmail.com>
2024-02-09 02:12:43 -0300

    Add dependencies and update global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
       },
+      colors: {
+        'blue': '#023E8A',
+      },
+      animation: {
+        appear: 'appear 2s ease-in',
+      },
+      keyframes: {
+        appear: {
+          '0%': { opacity: "0" },
+          '50%': { opacity: "0" },
+          '100%': { opacity: "1" },
+        }
+      }
     },
   },
   plugins: [],
