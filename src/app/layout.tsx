4a7e992d57948bc338bcafb5e10a181a77599f3f
Santiago Hanine <santihanine@gmail.com>
2024-02-10 05:14:03 -0300

    fix og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   keywords: "marketing digital, branding, creadores de contenido, estrategias digitales, agencia de marketing",
   category: "Marketing",
   openGraph:{
-    url: '/logo.jpg',
+    url: "https://middleman-branding.vercel.app/logo.jpg",
   },
 }
 
