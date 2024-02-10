8e673846983b97757fe9d047236ca15d781f4d54
Santiago Hanine <santihanine@gmail.com>
2024-02-10 04:56:29 -0300

    remove bg fixed on testimonials

--- src/app/Components/Testimonials/index.tsx
+++ src/app/Components/Testimonials/index.tsx
@@ -28,7 +28,7 @@ const Testimonials = ({ testimonials }: TestimonialsProps) => {
 
     return (
         <>
-        <div className="bg-office bg-fixed 2xl:bg-local h-screen bg-no-repeat bg-center flex justify-center items-center mb-20 px-6 md:px-10">
+        <div className="bg-office 2xl:bg-local h-screen bg-no-repeat bg-center flex justify-center items-center mb-20 px-6 md:px-10">
             <div className='relative bg-[rgba(255,255,255,0.85)] flex justify-center items-center h-[30rem] lg:h-[20rem] pt-20 pb-12 px-10 md:px-16'>
                 <Image data-aos="fade-up"  data-aos-duration="300" className='absolute top-4 left-2' src={Quote} alt='Quote sign'/>
                     <p className={`${isVisible ? styles.appear : styles.dissapear} text-xl lg:text-3xl text-black font-black md:font-semibold`}>{testimonials[currentTestimonialIndex]}</p>
