44de6aefafd6352c5652e07d2bfba81c8ae159ef
Santiago Hanine <santihanine@gmail.com>
2024-02-13 03:07:09 -0300

    Refactor Hero component styles

--- src/app/Components/Hero/index.tsx
+++ src/app/Components/Hero/index.tsx
@@ -11,7 +11,7 @@ const Hero = ({bgImage, title, text}:HeroProps) => {
   return (
     <div className={`${bgImage ? "bg-officeHero bg-cover bg-no-repeat h-[calc(100vh-5rem)] text-white" : "" }`}>
         <div className='h-full flex justify-center items-center'>
-        <div className={`flex flex-col text-center ${bgImage ? "mb-28 border-x-4 border-x-white px-4 md:pt-20 pt-24 pb-8 mx-4 md:mx-12 lg:mx-24 gap-10 md:gap-14" : "border-x-4 border-x-blue px-4 md:mt-20 mt-14 pt-8 pb-8 mx-4 md:mx-12 lg:mx-24 gap-10 md:gap-14"}`}>
+        <div className={`flex flex-col text-center ${bgImage ? "border-x-4 border-x-white px-4 md:pt-20 pt-24 pb-8 mx-4 md:mx-12 lg:mx-24 gap-10 md:gap-14" : "border-x-4 border-x-blue px-4 md:mt-20 mt-14 pt-8 pb-8 mx-4 md:mx-12 lg:mx-24 gap-10 md:gap-14"}`}>
 
         <h1 data-aos="fade-up" data-aos-duration="600" className='max-w-[55ch] mx-auto text-2xl md:text-4xl lg:text-5xl font-black'>
             {title}
