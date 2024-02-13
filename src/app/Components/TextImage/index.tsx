3a1f4e18b41d0d105dd8b6d6b7882c0d77f977d4
Santiago Hanine <santihanine@gmail.com>
2024-02-13 18:27:06 -0300

    Update TextImage component styling

--- src/app/Components/TextImage/index.tsx
+++ src/app/Components/TextImage/index.tsx
@@ -14,15 +14,15 @@ const TextImage = ({bgBlue, title, text, rowReverse, imgSrc, imgAlt}:TextImage) 
         <div className={`${bgBlue ? "bg-blue text-white" : "bg-white"} ${rowReverse ? "flex-col lg:flex-row-reverse" : "flex-col lg:flex-row"} flex items-center gap-8 lg:gap-20 p-10 lg:p-28`}>
                 <div className="w-full flex flex-col gap-8"
                     >
-                        <h2 data-aos="fade-up" data-aos-delay="400" className='text-3xl md:text-4xl lg:text-3xl font-black'>{title}</h2>
-                        <p data-aos="fade-up" data-aos-delay="400" className='text-lg lg:w-11/12 leading-9'>
+                        <h2 data-aos="fade-up" data-aos-delay="200" className='text-3xl md:text-4xl lg:text-3xl font-black'>{title}</h2>
+                        <p data-aos="fade-up" data-aos-delay="200" className='text-lg lg:w-11/12 leading-9'>
                                 {text}
                         </p>
                 </div>
 
                 <Image 
                     data-aos="fade-up"
-                    data-aos-delay="800"
+                    data-aos-delay="500"
                     src={imgSrc} 
                     alt={imgAlt} 
                     width={600} 
