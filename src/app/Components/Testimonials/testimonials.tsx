0f4c8e018139053c4349ffb13ca97c5a0bc1b2e3
Santiago Hanine <santihanine@gmail.com>
2024-02-13 03:43:01 -0300

    Import testimonials data

---
+++ src/app/Components/Testimonials/testimonials.tsx
@@ -0,0 +1,7 @@
+export const testimonials = [
+    "MiddleMan Branding ha sido una verdadera bendición para mi negocio. Gracias a su enfoque estratégico y su compromiso con el éxito de sus clientes, he podido aumentar significativamente mi presencia en línea y generar más ventas. ¡Altamente recomendado!",
+
+    "Honestamente nosotros veníamos pagando anuncios en Instagram y nunca vimos un efecto significativo por lo que pagábamos, ahí es cuando probamos con Middleman y si nos dio el empujón que necesitábamos.",
+
+    "Siempre me gustó mucho la moda y hacer contenido de esto, pero pensaba que para poder llegar a ganar plata haciendo esto me faltaba mucho tiempo, pero con middleman pude empezar a monetizar mis view al segundo."
+];
