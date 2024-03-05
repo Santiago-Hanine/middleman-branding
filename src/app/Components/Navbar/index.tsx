96d18aef567afe35fc3ad024f4ac8499d4d75569
Santiago Hanine <santihanine@gmail.com>
2024-03-05 14:02:40 -0300

    Update Navbar styling

--- src/app/Components/Navbar/index.tsx
+++ src/app/Components/Navbar/index.tsx
@@ -32,11 +32,11 @@ const Navbar = () => {
       </Link>
 
       <nav>
-        <ul id='menu-mobile' className='hidden text-center lg:flex flex-col lg:flex-row items-start lg:items-center justify-start lg:static absolute top-20 left-0 h-screen bg-white lg:bg-transparent gap-20 lg:gap-10 z-50 w-full pl-8 pt-20 pb-20 lg:pt-0 lg:pl-0 lg:pb-0 text-3xl font-black lg:text-base'>
+        <ul id='menu-mobile' className='hidden text-center lg:flex flex-col lg:flex-row items-start lg:items-center justify-start lg:static absolute top-20 left-0 h-screen bg-white lg:bg-transparent gap-20 lg:gap-10 z-50 w-full pl-8 pt-20 pb-20 lg:pt-0 lg:pl-0 lg:pb-0 text-2xl md:text-3xl font-black lg:text-base'>
           {Navlist.map((item, index) => (
-            <li className='text-left' key={index}>
+            <li key={index}>
               <Link 
-               className={pathname === item.link ? 'underline decoration-blue decoration-2 underline-offset-4 text-left' : 'text-left'}
+               className={pathname === item.link ? 'underline decoration-blue decoration-2 underline-offset-4' : ''}
                onClick={handleMenu} 
                href={item.link}>
                 {item.name}
